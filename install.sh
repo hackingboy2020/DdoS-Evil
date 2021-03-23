69512bcf0d6acf6266d3b2b9bb82a4eda38a5aea
hackingboy2020 <79807496+hackingboy2020@users.noreply.github.com>
2021-03-23 18:34:50 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt-get install xterm -y 
+sudo apt-get install python -y 
+sudo apt-get install python2 -y 
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+sudo apt-get install php
