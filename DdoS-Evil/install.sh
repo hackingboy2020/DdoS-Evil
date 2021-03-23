dac1052467677288f50238209290adfeb5c35c45
hackingboy2020 <79807496+hackingboy2020@users.noreply.github.com>
2021-03-23 17:33:41 +0000

    Delete DdoS-Evil directory

--- DdoS-Evil/install.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get install xterm -y 
-sudo apt-get install python -y 
-sudo apt-get install python2 -y 
-sudo apt-get install python3 -y
-sudo apt-get install python3-pip -y
-sudo apt-get install php
